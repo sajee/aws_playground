36612c231ae8ef7748e70b4f7186058ee68eeff5
Sajee Mathew <sajee@sajee.net>
2016-08-08 14:17:59 -0400

    Updated snapshot out of date flag.

--- snapshot-report.py
+++ snapshot-report.py
@@ -28,11 +28,11 @@ for instance in instances:
             print(': Snapshot:' + snapshot.id+ "  " + str(snapshot_date), end = '')
             # Can't compare TZ naive datatime.now to TZ aware snapshot date.
             # Removed timezone from snapshot timestamp (which is UTC).   Compare against current UTC time.
-            snapshot_older_than_a_day = snapshot_date.replace(tzinfo=None) < datetime.utcnow() - timedelta(hours=SNAPSHOT_AGE_IN_HOURS)
+            snapshot_out_of_date = snapshot_date.replace(tzinfo=None) < datetime.utcnow() - timedelta(hours=SNAPSHOT_AGE_IN_HOURS)
             snapshot_delta = datetime.utcnow().replace(microsecond=0) - snapshot_date.replace(tzinfo=None).replace(microsecond=0)
             snapshot_age_msg = str(snapshot_delta) + " since last snapshot."
 
-            if snapshot_older_than_a_day:
+            if snapshot_out_of_date:
                 print("\t*** WARNING: This snapshot is out of date.\t" + snapshot_age_msg)
             else:
                 print("\t@@@ This snapshot is current.\t\t\t\t" + snapshot_age_msg)
