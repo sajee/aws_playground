0b01adf68fa0882e369db4330f0a4ccd2cc65847
Sajee Mathew <sajee@sajee.net>
2016-08-07 21:59:38 -0400

    cleanup

--- snapshot-report.py
+++ snapshot-report.py
@@ -1,5 +1,5 @@
 '''
-Display instances that have volumes that have no snapshots or
+Per instance, display volumes with no snapshots or
 volumes that have not been snapshotted recently.
 '''
 
@@ -11,11 +11,10 @@ ec2 = boto3.resource('ec2')
 instance_name_tag = 'Name'
 instances = ec2.instances.all() # .filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 for instance in instances:
-    print("\n")
     instance_name = ''
     for tag in instance.tags:
         instance_name = tag['Value'] if tag['Key'] == instance_name_tag else None
-    print("Instance name: '%s' \t Instance ID: %s." %(instance_name, instance.id))
+    print("\n\nInstance name: '%s' \t Instance ID: %s." %(instance_name, instance.id))
 
     for volume in instance.volumes.all():
         print("Volume ID: %s" % (volume.id))
@@ -25,8 +24,8 @@ for instance in instances:
             snapshot_taken = True
             snapshot_date = snapshot.start_time
             print(': Snapshot:' + snapshot.id+ "  " + str(snapshot_date), end = '')
-            # FIX: datatime.now doesn't have TZ info which causes an error when compared to snapshot date.
-            # Removed timezone foe now.  Need to fix.
+            # FIX: can't compare TZ naive datatime.now to TZ aware snapshot date.
+            # Removed timezone from snapshot date for now.  Need to fix because the comparison should be wrong by a few hours.
             snapshot_older_than_a_day = snapshot_date.replace(tzinfo=None) < datetime.now() - timedelta(days=1)
             if snapshot_older_than_a_day:
                 print("\t*** WARNING: This snapshot is out of date.")
