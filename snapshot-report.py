bfa94501539c1aeeee625650a099b11c351d1a79
Sajee Mathew <sajee@sajee.net>
2016-08-16 11:42:24 -0400

    RequestLimitExceeded caught and simple sleep added.

--- snapshot-report.py
+++ snapshot-report.py
@@ -4,15 +4,18 @@ volumes that have not been snapshotted recently.
 '''
 
 import boto3
-from datetime import datetime, timedelta
+import botocore
 
+from datetime import datetime, timedelta
+import time
 
 INSTANCE_NAME_TAG = 'Name'
 SNAPSHOT_AGE_IN_HOURS = 24
-
+SLEEP_IN_SECONDS = 5
 
 def check_volumes():
     ec2 = boto3.resource('ec2')
+
     instances = ec2.instances.all()  # .filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     for instance in instances:
         instance_name = ''
@@ -20,28 +23,41 @@ def check_volumes():
             instance_name = tag['Value'] if tag['Key'] == INSTANCE_NAME_TAG else None
         print("\n\nInstance name: '%s' \t Instance ID: %s" % (instance_name, instance.id))
 
-        for volume in instance.volumes.all():
-            print("Volume ID: %s" % (volume.id))
-            snapshot_taken = False
+        while True:  # in case of an exception, redo volume or snapshot check.  See http://stackoverflow.com/questions/2083987/how-to-retry-after-exception-in-python
+            try:
+                for volume in instance.volumes.all():
+                    print("Volume ID: %s" % (volume.id))
+                    snapshot_taken = False
 
-            for snapshot in volume.snapshots.all():
-                snapshot_taken = True
-                snapshot_date = snapshot.start_time
-                print(': Snapshot:' + snapshot.id + "  " + str(snapshot_date), end = '')
-                # Can't compare TZ naive datatime.now to TZ aware snapshot date.
-                # Removed timezone from snapshot timestamp (which is UTC).   Compare against current UTC time.
-                snapshot_out_of_date = snapshot_date.replace(tzinfo=None) < datetime.utcnow() - timedelta(
-                    hours=SNAPSHOT_AGE_IN_HOURS)
-                snapshot_delta = datetime.utcnow().replace(microsecond=0) - snapshot_date.replace(tzinfo=None).replace(
-                    microsecond=0)
-                snapshot_age_msg = str(snapshot_delta) + " since last snapshot."
+                    for snapshot in volume.snapshots.all():
+                        snapshot_taken = True
+                        snapshot_date = snapshot.start_time
+                        print(': Snapshot:' + snapshot.id + "  " + str(snapshot_date), end = '')
+                        # Can't compare TZ naive datatime.now to TZ aware snapshot date.
+                        # Removed timezone from snapshot timestamp (which is UTC).   Compare against current UTC time.
+                        snapshot_out_of_date = snapshot_date.replace(tzinfo=None) < datetime.utcnow() - timedelta(
+                            hours=SNAPSHOT_AGE_IN_HOURS)
+                        snapshot_delta = datetime.utcnow().replace(microsecond=0) - snapshot_date.replace(tzinfo=None).replace(
+                            microsecond=0)
+                        snapshot_age_msg = str(snapshot_delta) + " since last snapshot."
 
-                if snapshot_out_of_date:
-                    print("\t*** WARNING: This snapshot is out of date.\t" + snapshot_age_msg)
+                        if snapshot_out_of_date:
+                            print("\t*** WARNING: This snapshot is out of date.\t" + snapshot_age_msg)
+                        else:
+                            print("\t@@@ This snapshot is current.\t\t\t\t" + snapshot_age_msg)
+
+                    if not snapshot_taken:
+                        print(":\t *** WARNING: No snapshots taken for this volume.")
+
+            except botocore.exceptions.ClientError as e:
+                if e.response['Error']['Code'] == 'RequestLimitExceeded':
+                    #print("^^^^^^^^^^   REQUEST LIMIT EXCEEDED ^^^^^^^^^^^^^: %s" % e)
+                    time.sleep(SLEEP_IN_SECONDS) # boto's built-in exponential backoff wasn't good enough so just sleep
                 else:
-                    print("\t@@@ This snapshot is current.\t\t\t\t" + snapshot_age_msg)
-            if not snapshot_taken:
-                print(":\t *** WARNING: No snapshots taken for this volume.")
+                    print("%%%%%%%%%%%%%%%%%%%% Unexpected error: %s" % e)
+                continue
+            break
+
 
 
 check_volumes()
